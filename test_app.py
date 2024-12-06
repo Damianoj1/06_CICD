44d4c60f49a511edd44b3d3d82333b095aa823f1
biur.dj@dj.com <biur.dj@dj.com>
2024-12-06 09:48:35 +0100

    tets06CICD

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import unittest
+from app import hello_world
+
+class TestApp(unittest.TestCase):
+    def test_hello(self)
+        self.assertAlmostEqual(hello_world(),"|")
+        
+ if __name__ == "__main__":
+    unittest.main()
