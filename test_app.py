5d8bc96b7593b2eab76d9d172df8f8db9127c8e6
biur.dj@dj.com <biur.dj@dj.com>
2024-12-06 11:55:34 +0100

    zmiana modulu

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import hello_world
 
 class TestApp(unittest.TestCase):
     def test_hello(self):
-        self.assertAlmostEqual(hello_world(),"Hello_world")
+        self.assertEqual(hello_world(),"Hello_world")
         
 if __name__ == "__main__":
     unittest.main()
