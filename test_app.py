8b1a2b9954966868200eb6f64522cd5b787b2830
biur.dj@dj.com <biur.dj@dj.com>
2024-12-06 12:56:15 +0100

    dodnaie 3

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import hello_world
 
 class TestApp(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello_world(), "Hello World!")
+        self.assertEqual(hello_world(), "Hello World!3")
         
 if __name__ == "__main__":
     unittest.main()
