02601b1ec8e3cd31e8feb50c9191a054f270b1da
biur.dj@dj.com <biur.dj@dj.com>
2024-12-06 11:17:23 +0100

    dodalem pliki docker jenkins

---
+++ main.tf
@@ -0,0 +1,41 @@
+# Base image
+FROM python:3.12-slim
+
+# Set the working directory
+WORKDIR /app
+
+# Copy application files
+COPY . /app
+
+# Install dependencies
+RUN pip install -r requirements.txt
+
+# Run the application
+CMD ["python", "app.py"]
+
+provider "aws" {
+region = var.region
+}
+
+resource "aws_instance" "app" {
+ami = var.ami_id
+instance_type = "t2.micro"
+
+tags = {
+Name = "PythonAppInstance"
+}
+
+provisioner "remote-exec" {
+inline = [
+"docker run -d -p 80:80 python-app"
+]
+}
+}
+
+variable "region" {
+default = "us-east-1"
+}
+
+variable "ami_id" {
+default = "ami-0c02fb55956c7d316" # AMI z Linux
+}
--- test_app.py
+++ test_app.py
@@ -2,8 +2,8 @@ import unittest
 from app import hello_world
 
 class TestApp(unittest.TestCase):
-    def test_hello(self)
-        self.assertAlmostEqual(hello_world(),"|")
+    def test_hello(self):
+        self.assertAlmostEqual(hello_world(),"Hello_world")
         
  if __name__ == "__main__":
     unittest.main()
